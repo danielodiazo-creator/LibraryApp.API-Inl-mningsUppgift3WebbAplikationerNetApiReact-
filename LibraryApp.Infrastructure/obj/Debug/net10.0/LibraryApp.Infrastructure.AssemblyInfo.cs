52e3418a24e0500e423331cd627a5a6a84a36f00
danielodiazo-creator <danielodiazo@gmail.com>
2026-05-13 09:44:15 +0200

    Försöker fixa Git

--- LibraryApp.Infrastructure/obj/Debug/net10.0/LibraryApp.Infrastructure.AssemblyInfo.cs
+++ LibraryApp.Infrastructure/obj/Debug/net10.0/LibraryApp.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7950b91a5253ecb97530f41e41ab7114283f7d90")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
